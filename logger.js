13d98f8c236347b06a11bc39539edf18eda3e11d
Diego Dos Santos <diegodossantos@MacBook-Pro.home>
2020-12-17 12:09:07 +0100

    end logger

--- logger.js
+++ logger.js
@@ -2,14 +2,20 @@ const EventEmitter = require ('events')
 const fs = require('fs')
 const path = require('path')
 
+// estancia el new pues usaré una nueva clase
 const emitter = new EventEmitter()
 
+//emitter on activa la escucha y en el según parametro estancia la respuesta
+//appendFile va a leer el archivo 
+//path join buscará el archivo en cualquier directorio
+//passando el messsage y la function de error 
 emitter.on('log', (message) => {
     fs.appendFile(path.join(__dirname, 'log.txt'), message, err => {
         if(err) throw err;
     })
 })
 
+//creamos la function log para exportar 
 function log(message){
     emitter.emit('log', message)
 }
