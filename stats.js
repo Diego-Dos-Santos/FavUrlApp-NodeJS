ccfc300f4377024220943be9f2e2ef3f98ea5b1f
Diego Dos Santos <diegodossantos@MacBook-Pro.home>
2020-12-17 11:25:09 +0100

    end stats

---
+++ stats.js
@@ -0,0 +1,21 @@
+// APP MEMORIA ( Pequeña para saber la cantidad de memoria y el porcetaje de la misma )
+
+const os = require("os");
+
+//Función Global
+setInterval(() => {
+  // Desestructurar la variable al revés de dos, instaciamos en una
+  const { freemem, totalmem } = os;
+
+  const mem = parseInt(freemem() / 1024 / 1024);
+  const total = parseInt(totalmem() / 1024 / 1024);
+  const percents = parseInt((mem / total) * 100);
+
+  const stats = {
+    free: `${mem} MB`,
+    total: `${total} MB`,
+    usage: `${percents} %`,
+  };
+  console.clear()
+  console.table(stats);
+}, 1000);
